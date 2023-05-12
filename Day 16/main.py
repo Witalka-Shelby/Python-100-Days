ba8f078c174723434434546538f6285994eba46a
Witalka <witalka15@gmail.com>
2023-05-12 11:59:44 -0400

    shorten day 16

--- Day 16/main.py
+++ Day 16/main.py
@@ -6,34 +6,23 @@ coffe_maker = CoffeeMaker()
 menu = Menu()
 money_machine = MoneyMachine()
 
-coffee_list = menu.get_items()
 is_on = True
 
 
 while is_on:
+    coffee_list = menu.get_items()
     coffee_choice = input(f"What would you like? ({coffee_list}): ").lower()
 
     if coffee_choice == "report":
         coffe_maker.report()
         money_machine.report()
-        continue
         
     elif coffee_choice == "off":
         is_on = False
-        continue
 
-    user_choice = menu.find_drink(coffee_choice)
-    
-    if user_choice == None:
-        continue
-    
-    check_resources = coffe_maker.is_resource_sufficient(user_choice)
-    
-    if check_resources == False:
-        continue
+    else:
+        user_choice = menu.find_drink(coffee_choice)
+        coffee_cost = user_choice.cost
 
-    coffee_cost = user_choice.cost
-    user_payed = money_machine.make_payment(coffee_cost)
-    
-    if user_payed:
-        coffe_maker.make_coffee(user_choice)
+        if coffe_maker.is_resource_sufficient(user_choice) and money_machine.make_payment(coffee_cost):
+            coffe_maker.make_coffee(user_choice)
