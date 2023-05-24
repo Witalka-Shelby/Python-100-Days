aa46432c7df6bbc739d3f05c2f58c01dd3b5a89b
Witalka Shelby <witalka15@gmail.com>
2023-05-24 16:55:34 -0400

    26.4 & 26.5

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,5 +1,12 @@
+import random
+
 names = ["Alex", "Beth", "Caroline", "Eleanor", "Dave", "Freddie"]
 
 upper_case_list = [name.upper() for name in names if len(name) > 4]
 
-print(upper_case_list)
+students_score = {name:random.randint(0, 100) for name in names}
+
+passed_students = {student:students_score[student] for student in students_score if students_score[student] > 60}
+
+print(students_score)
+print(passed_students)
