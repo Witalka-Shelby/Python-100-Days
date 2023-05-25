d1fb0fd71c6ad259b7e2f332a45cccee04b72812
Witalka Shelby <witalka15@gmail.com>
2023-05-25 11:57:33 -0400

    day 27

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,13 +1,31 @@
-import tkinter
+from tkinter import *
+window = Tk()
+window.title("Mil3 to Km Converter")
+window.minsize(width=100, height=50)
+window.config(padx=20, pady=20)
 
-window = tkinter.Tk()
-window.title("My first GUI App")
-window.minsize(width=500, height=300)
-
-my_label = tkinter.Label(text="MY LABEL")
-my_label.pack()
+def convert_to_km():
+    miles = float(user_input.get())
+    kms = round(miles * 1.609, 2)
+    label_converted.config(text=f"{kms}")
 
 
+button = Button(text="Calculate", command=convert_to_km)
+button.grid(column=1, row=2)
 
+label1 = Label(text="is equal to")
+label1.grid(column=0, row=1)
 
-window.mainloop()
+label_miles = Label(text="Miles")
+label_miles.grid(column=2, row=0)
+
+label_km = Label(text="Km")
+label_km.grid(column=2, row=1)
+
+user_input = Entry(width=10)
+user_input.grid(column=1, row=0)
+
+label_converted = Label(text="0")
+label_converted.grid(column=1, row=1)
+
+window.mainloop() 
--- Day 27/playground.py
+++ Day 27/playground.py
@@ -1,8 +1,35 @@
-def add(*args):
-    total = 0
-    for arg in args:
-        total += arg
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
 
-    print(total)
+#     print(total)
 
-add(5, 8, 5, 2, 3, 6, 4 ,7, 7)
+# add(5, 8, 5, 2, 3, 6, 4 ,7, 7)
+
+# def calculate(**kwargs):
+
+
+from tkinter import *
+window = Tk()
+window.title("My first GUI App")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=120)
+
+def button_click():
+    new_label = user_input.get()
+    my_label.config(text=new_label)
+
+my_label = Label(text="MY LABEL")
+my_label.grid(column=0, row=0)
+
+button = Button(text="Click ME !!!", command=button_click)
+button.grid(column=2, row=1)
+
+button2 = Button(text="NEW BUTTON", command=button_click)
+button2.grid(column=3, row=0)
+
+user_input = Entry(width=20)
+user_input.grid(column=4, row=3)
+
+window.mainloop() 
