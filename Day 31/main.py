6040e02086a4738382f2d60040f9d28d7a5db322
Witalka Shelby <witalka15@gmail.com>
2023-05-31 14:48:12 -0400

    day 31 step 3

--- Day 31/main.py
+++ Day 31/main.py
@@ -3,29 +3,43 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+timer = None
+current_card = {}
 
 words_csv = pandas.read_csv("./day 31/data/french_words.csv")
 words_dict = words_csv.to_dict(orient="records")
 
 def get_word():
-    global words_dict
-    random_word = random.choice(words_dict)
-    test_word = random.choice([random_word["French"], random_word["English"]])
-    # print(random_word["French"])
-    # print(random_word["English"])
-    canvas.itemconfigure(show_word, text=test_word)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(words_dict)
+    canvas.itemconfig(canvas_background, image=card_front_png)
+    canvas.itemconfigure(card_language, text="French", fill="black")
+    canvas.itemconfigure(card_word, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000, reveal_card)
+
+    
+def reveal_card():
+    global current_card
+    canvas.itemconfig(canvas_background, image=card_back_png)
+    canvas.itemconfig(card_language, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    
+    
 
 window = Tk()
 window.title("Fla5hy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, reveal_card)
+
 # Card Part
 canvas = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_png = PhotoImage(file="./day 31/images/card_front.png")
 card_back_png = PhotoImage(file="./day 31/images/card_back.png")
-canvas.create_image(400, 263, image=card_front_png)
-show_language = canvas.create_text(400, 150, text="French", font=("Ariel", "40", "italic"))
-show_word = canvas.create_text(400, 263, font=("Ariel", "40", "italic"))
+canvas_background = canvas.create_image(400, 263, image=card_front_png)
+card_language = canvas.create_text(400, 150, text="French", font=("Ariel", "40", "italic"))
+card_word = canvas.create_text(400, 263, font=("Ariel", "40", "italic"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Wrong Button
