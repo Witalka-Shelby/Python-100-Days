ae8a02d9f43235e23df4cbb98baf0d54e2cc0f99
Witalka Shelby <witalka15@gmail.com>
2023-06-02 11:42:33 -0400

    day 35

--- Day 35/main.py
+++ Day 35/main.py
@@ -39,20 +39,20 @@ for i in range(4):
 #         print("~12h !!!")
 
 # Download the helper library from https://www.twilio.com/docs/python/install
-import os
-from twilio.rest import Client
+# import os
+# from twilio.rest import Client
 
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
-message = client.messages \
-                .create(
-                    body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                    from_='+133333336',
-                    to='+133333337'
-                )
-if will_rain:
-    print(message.sid)
+# # Find your Account SID and Auth Token at twilio.com/console
+# # and set the environment variables. See http://twil.io/secure
+# account_sid = os.environ['TWILIO_ACCOUNT_SID']
+# auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# client = Client(account_sid, auth_token)
+# message = client.messages \
+#                 .create(
+#                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+#                     from_='+133333336',
+#                     to='+133333337'
+#                 )
+# if will_rain:
+#     print(message.sid)
