63e23bcf82f5ce5b708b580dfcb778977de0b8f4
Witalka Shelby <witalka15@gmail.com>
2023-06-02 10:47:48 -0400

    challenge 12 hours

--- Day 35/main.py
+++ Day 35/main.py
@@ -17,11 +17,23 @@ today = datetime.now()
 
 
 response = requests.get(url)
-print(response.status_code)
+# print(response.status_code)
 response.raise_for_status()
-weather_data_json = response.json()
-for weather in weather_data_json['list']:
-    weather_date = weather['dt_txt'].split(" ")[0].split("-")[2]
-    if (today.day + 2) == int(weather_date):
-        print("48h !!!")
+weather_data_json = response.json()['list']
+
+for i in range(4):
+    weather_date = int(weather_data_json[i]["dt_txt"].split(" ")[0].split("-")[2])
+    weather_date_time = int(weather_data_json[i]["dt_txt"].split(" ")[1].split(":")[0])
+    weather_id = weather_data_json[i]["weather"][0]["id"]
+    if weather_id < 700:
+        print("Bring an umbrella")
+        print(weather_data_json[i]["weather"][0]["description"])
+        print(weather_data_json[i]["dt_txt"])
+
+# for weather in weather_data_json:
+#     weather_date = int(weather['dt_txt'].split(" ")[0].split("-")[2])
+#     weather_date_time = int(weather['dt_txt'].split(" ")[1].split(":")[0])
+#     if (weather_date_time-1)  <= (today.hour + 12) <= (weather_date_time+1) and weather_date == today.day:
+#         print(weather["weather"][0]["description"])
+#         print("~12h !!!")
 
