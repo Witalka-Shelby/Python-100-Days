5db35b4aaffb29b61f7185ca72688e66d3e23869
Witalka Shelby <witalka15@gmail.com>
2023-06-06 12:57:22 -0400

    step 4

--- Day 39/flight_data.py
+++ Day 39/flight_data.py
@@ -1,5 +1,7 @@
 from dotenv import dotenv_values
 import requests
+from datetime import datetime
+from datetime import timedelta
 
 config = dotenv_values(".env")
 KIWI_API = config['KIWI_API']
@@ -7,19 +9,33 @@ KIWI_API = config['KIWI_API']
 class FlightData:
 
     def __init__(self) -> None:
-        header = {"apikey": KIWI_API}
-        kiwi_config = {
-            "fly_from": "LGA",
-            "fly_to": "MIA",
-            "date_from": "01/11/2023",
-            "date_to": "24/11/2023",
+        self.header = {"apikey": KIWI_API}
+        today = datetime.today()
+        self.tomorrow = today + timedelta(days= 1)
+        self.half_year = self.tomorrow + timedelta(days=180)
+        # print(self.tomorrow.strftime("%d/%m/%Y"))
+        # "01/11/2023"
+    
+    def search_fligh(self, departure_airport_code, fly_to, lowest_price):
+        config = {
+            "fly_from": departure_airport_code,
+            "fly_to": fly_to,
+            "date_from": self.tomorrow.strftime("%d/%m/%Y"),
+            "date_to": self.half_year.strftime("%d/%m/%Y"),
+            "max_stopovers": 0,
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
             "curr": "USD"
         }
     
         kiwi_url = "https://api.tequila.kiwi.com/v2/search"
-        response = requests.get(url=kiwi_url, headers=header, params=kiwi_config)
-
+        response = requests.get(url=kiwi_url, headers=self.header, params=config)
         response.raise_for_status()
-        # print(response.text)
+        flights = response.json()["data"]
+        for flight in flights:
+            airport = flight["cityTo"]
+            price = flight["price"]
+            if price <= lowest_price:
+                print(f"{airport}: {'${}'.format(price)}")
 
-FlightData()
+# FlightData()
--- Day 39/main.py
+++ Day 39/main.py
@@ -1,21 +1,28 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
+FLY_FROM = "TPA"
+
+
 dm = DataManager()
 fs = FlightSearch()
+fd = FlightData()
 
 sheet_data = dm.get_destenations()
 
+# updates the missing IATA codes
 for place in sheet_data:
-    if place["iataCode"] == "":
-        city_id = place["id"]
-        city_name = place["city"]
+    city_id = place["id"]
+    city_name = place["city"]
+    iata_code = place["iataCode"]
+    lowest_price = place["lowestPrice"]
+
+    if iata_code == "":
         iata_code = fs.iata_code(city_name)
-        
         dm.update_iata_codes(id=city_id, iata=iata_code)
 
+    fd.search_fligh(departure_airport_code=FLY_FROM, fly_to=iata_code, lowest_price=lowest_price)
 
-
-# print(sheet_data)
