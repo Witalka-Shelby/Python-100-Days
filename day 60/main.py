16c3cdfb40e12e01db3f05af891a2654e9092dd5
Witalka Shelby <witalka15@gmail.com>
2023-06-22 18:57:45 -0400

    day 60 step 2

--- day 60/main.py
+++ day 60/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    return "Hello submit"
 
 
 if __name__ == "__main__":
