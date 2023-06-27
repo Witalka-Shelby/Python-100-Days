66bb5ef37a9aba48036e3910767840c0ddb98ea4
Witalka Shelby <witalka15@gmail.com>
2023-06-27 08:58:14 -0400

    step 2

--- day 61/main.py
+++ day 61/main.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = "1337"
 
+class MyForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    form = MyForm()
+    email = form.email.data
+    password = form.password.data
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
