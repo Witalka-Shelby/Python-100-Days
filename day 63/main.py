84a48c0ff22c6ac4ee115015d582fc91b797c167
Witalka Shelby <witalka15@gmail.com>
2023-06-28 13:59:51 -0400

    day 63 final

--- day 63/main.py
+++ day 63/main.py
@@ -3,6 +3,51 @@ import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
 
+def book_to_db(name, author, rating):
+    ### add book to db
+    book = Books(title = name, author = author, rating = rating)
+
+    with app.app_context():
+        db.session.add(book)
+        db.session.commit()
+
+
+def read_all_db():
+    with app.app_context():
+        result = db.session.execute(db.select(Books).order_by(Books.title))
+        all_books = result.scalars()
+        book_list = []
+        for book in all_books:
+            book_list.append({
+                "id": book.id,
+                "title": book.title,
+                "author": book.author,
+                "rating": book.rating
+                })
+        return book_list
+    
+
+def find_book_in_db(id):
+    with app.app_context():
+        result = db.session.execute(db.select(Books).where(Books.id == id))
+        book = result.scalar()
+        return book
+
+
+def update_rating(id, new_rating):
+    with app.app_context():
+        book_to_update = db.session.execute(db.select(Books).where(Books.id == id)).scalar()
+        book_to_update.rating = new_rating
+        db.session.commit()
+
+
+def delete_book(id):
+    with app.app_context():
+        book_to_del = db.session.execute(db.select(Books).where(Books.id == id)).scalar()
+        db.session.delete(book_to_del)
+        db.session.commit()
+
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,24 +63,10 @@ class Books(db.Model):
 with app.app_context():
     db.create_all()
 
-all_books = []
-
-def book_to_db():
-    book = Books(
-        id = 1,
-        title = "Harry Potter",
-        author = "JK Roling",
-        rating = 9.1
-    )
-
-    with app.app_context():
-        db.session.add(book)
-        db.session.commit()
-
-book_to_db()
 
 @app.route('/')
 def home():
+    all_books = read_all_db()
     return render_template("index.html", books=all_books)
 
 
@@ -45,13 +76,25 @@ def add():
         name = request.form["book_name"]
         author = request.form["book_author"]
         rating = request.form["rating"]
-        all_books.append({
-            "title": name,
-            "author": author,
-            "rating": rating
-            })
+        book_to_db(name, author, rating)
+        return redirect(url_for("home"))
+    
     return render_template("add.html")
 
+@app.route("/edit/<id>", methods=["GET", "POST"])
+def edit(id):
+    if request.method == "POST":
+        new_rating = request.form["new_rating"]
+        update_rating(id, new_rating)
+        return redirect(url_for("home"))
+
+    book = find_book_in_db(id)
+    return render_template("edit.html", book=book)
+
+@app.route("/del/<id>")
+def delete(id):
+    delete_book(id)
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run()
