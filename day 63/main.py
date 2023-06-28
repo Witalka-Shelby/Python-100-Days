8b38b3430c627bbec54f04126b81a1db23c28cd3
Witalka Shelby <witalka15@gmail.com>
2023-06-28 12:19:40 -0400

    SQLAlchemy challe

--- day 63/main.py
+++ day 63/main.py
@@ -1,8 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 all_books = []
+
+def book_to_db():
+    book = Books(
+        id = 1,
+        title = "Harry Potter",
+        author = "JK Roling",
+        rating = 9.1
+    )
+
+    with app.app_context():
+        db.session.add(book)
+        db.session.commit()
+
+book_to_db()
 
 @app.route('/')
 def home():
@@ -25,4 +55,3 @@ def add():
 
 if __name__ == "__main__":
     app.run()
-
