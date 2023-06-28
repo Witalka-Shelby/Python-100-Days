ef71376032f9cd7a14ab4a192948b2446278bb5d
Witalka Shelby <witalka15@gmail.com>
2023-06-28 09:13:39 -0400

    challenge 3

--- day 63/main.py
+++ day 63/main.py
@@ -4,14 +4,23 @@ app = Flask(__name__)
 
 all_books = []
 
-
 @app.route('/')
 def home():
+    print(all_books)
     return render_template("index.html")
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    if request.method == "POST":
+        name = request.form["book_name"]
+        author = request.form["book_author"]
+        rating = request.form["rating"]
+        all_books.append({
+            "title": name,
+            "author": author,
+            "rating": rating
+            })
     return render_template("add.html")
 
 
